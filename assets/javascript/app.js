554e3126785fddac00ef03f1eaf2fec5b36a57e0
Chris Armendarez <chris.michael.armo@gmail.com>
2018-08-29 21:56:36 -0700

    Add questions & answers to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+/**
+ * Metal-Trivica is a trivia game that will test a players knowledge of metal bands,
+ * past and present
+ */
+
+// GLOBAL VARIABLES
+// -------------------------------------------------->
+// Quiz questions and answers -->
+const quiz = [{
+    question: "When did Metallica release their first album?",
+    answer: "April 4, 1982",
+    incorrect: [
+        "March 30, 1983",
+        "November 6, 1983",
+        "August 10, 1982"
+    ]
+}, {
+    question: "Who was Metallica's first bass player?",
+    answer: "Cliff Burton",
+    incorrect: [
+        "Jason Newsted",
+        "Robert Trujillo",
+        "Steve Harris"
+    ]
+}, {
+    question: "What band came out with the album, Powerslave in 1984?",
+    answer: "Iron Maiden",
+    incorrect: [
+        "Black Sabbath",
+        "Metallica",
+        "Slayer"
+    ]
+}, {
+    question: "Who is the lead singer for Iron Maiden",
+    answer: "Bruce Dickinson",
+    incorrect: [
+        "James Hetfield",
+        "Paul Di' Anno",
+        "Paul Day"
+    ]
+}, {
+    question: "Metallica's song, 'The Four Horsemen' was composed by which Megadeth member?",
+    answer: "Dave Mustaine",
+    incorrect: [
+        "Nick Menza",
+        "Chris Poland",
+        "Marty Friedman"
+    ]
+}, {
+    question: "Megadeth's song, 'Dread and the Fugitive Mind' is on what album?",
+    answer: "The World Needs a Hero",
+    incorrect: [
+        "Rust in Peace",
+        "The System has Failed",
+        "Killing Is My Business... and Business Is Good!"
+    ]
+}, {
+    question: "What Metallica song was played at Cliff Burton's ceremony?",
+    answer: "Orion",
+    incorrect: [
+        "Fade to Black",
+        "Creeping Death",
+        "Welcome Home (Sanitarium)"
+    ]
+}];
+
+// DOM SELECTIONS
+// -------------------------------------------------->
+
+
+// FUNCTIONS
+// -------------------------------------------------->
+
+
+// PROCESS
+// -------------------------------------------------->
