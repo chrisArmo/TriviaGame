3442cce020b7bd03b9211686821a00b979250db2
Chris Armendarez <chris.michael.armo@gmail.com>
2018-08-30 23:12:52 -0700

    Add main game functionality to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,7 @@
 const quiz = [{
     question: "When did Metallica release their first album?",
     answer: "April 4, 1982",
+    image: "assets/images/power-metal.jpg",
     incorrect: [
         "March 30, 1983",
         "November 6, 1983",
@@ -17,6 +18,7 @@ const quiz = [{
 }, {
     question: "Who was Metallica's first bass player?",
     answer: "Cliff Burton",
+    image: "assets/images/cliff-burton.jpg",
     incorrect: [
         "Jason Newsted",
         "Robert Trujillo",
@@ -25,6 +27,7 @@ const quiz = [{
 }, {
     question: "What band came out with the album, Powerslave in 1984?",
     answer: "Iron Maiden",
+    image: "assets/images/iron-maiden-powerslave.jpg",
     incorrect: [
         "Black Sabbath",
         "Metallica",
@@ -33,6 +36,7 @@ const quiz = [{
 }, {
     question: "Who is the lead singer for Iron Maiden",
     answer: "Bruce Dickinson",
+    image: "assets/images/bruce-dickinson.png",
     incorrect: [
         "James Hetfield",
         "Paul Di' Anno",
@@ -41,6 +45,7 @@ const quiz = [{
 }, {
     question: "Metallica's song, 'The Four Horsemen' was composed by which Megadeth member?",
     answer: "Dave Mustaine",
+    image: "assets/images/dave-mustaine.jpg",
     incorrect: [
         "Nick Menza",
         "Chris Poland",
@@ -49,6 +54,7 @@ const quiz = [{
 }, {
     question: "Megadeth's song, 'Dread and the Fugitive Mind' is on what album?",
     answer: "The World Needs a Hero",
+    image: "assets/images/the-world-needs-a-hero.jpg",
     incorrect: [
         "Rust in Peace",
         "The System has Failed",
@@ -57,6 +63,7 @@ const quiz = [{
 }, {
     question: "What Metallica song was played at Cliff Burton's ceremony?",
     answer: "Orion",
+    image: "assets/images/metallica-orion.jpg",
     incorrect: [
         "Fade to Black",
         "Creeping Death",
@@ -67,15 +74,18 @@ game = {
     correctAnswers: 0,
     incorrectAnswers: 0,
     unansweredQuestions: 0,
+    questionIndex: 0,
     questionTime: 30,
     intervalId: null,
     over: false,
-    currentAnswer: null
+    currentAnswer: null,
+    currentImage: null
 };
 
 // DOM SELECTIONS
 // -------------------------------------------------->
-const startButton = $(".start"),
+const gameDisplay = $(".game"),
+startButton = $(".start"),
 timer = $(".timer");
 
 // FUNCTIONS
@@ -83,6 +93,36 @@ timer = $(".timer");
 // Get random array index -->
 function getRandomInd(arr) {
     return Math.floor(Math.random() * arr.length);
+}
+
+// Reset game state -->
+function gameReset() {
+    game.correctAnswers = 0;
+    game.incorrectAnswers = 0;
+    game.unansweredQuestions = 0;
+    game.questionIndex = 0;
+    game.currentAnswer = null;
+    game.currentImage = null;
+    game.intervalId = null;
+    game.over = false;
+}
+
+// Check game end -->
+function checkGameEnd() {
+    if (game.questionIndex === quiz.length) {
+        game.over = true;
+    }
+}
+
+// Show game end results -->
+function showGameEndResults() {
+    gameDisplay.empty();
+    gameDisplay.html(`
+        <h3>Correct Answers: ${game.correctAnswers}</h3>
+        <h3>Incorrect Answers: ${game.incorrectAnswers}</h3>
+        <h3>Unanswered: ${game.unansweredQuestions}</h3>
+        <button class="btn btn-lg btn-danger mt-3 play-again" type="button">Play Again</button>
+    `);
 }
 
 // Randomize array order & return new array -->
@@ -98,32 +138,113 @@ function randomizeArr(arr) {
 
 // Create & display question & answers -->
 function createAndDisplay(question) {
+    gameDisplay.empty();
     const $questionH3 = $(`<h3 class="mb-4 question">${question.question}</h3>`),
     $answersGroup = $("<ul class='list-group'></ul>"),
     randomIndex = getRandomInd(question.incorrect);
     answers = randomizeArr(question.incorrect);
     answers.splice(randomIndex, 0, question.answer);
     answers.forEach(answer => {
-        $answersGroup.append(`<li class="list-group-item answer">${answer}</li>`);
+        $answersGroup.append(
+            `<li class="list-group-item answer" data-answer="${answer}">${answer}</li>`
+        );
     });
-    $(".game").append($questionH3, $answersGroup);
+    gameDisplay.append($questionH3, $answersGroup);
+}
+
+// Display next question in five seconds -->
+function displayQuestionInFive() {
+    setTimeout(() => {
+        createAndDisplay(quiz[game.questionIndex]);
+        startAndUpdateTimer(game);
+        game.currentAnswer = quiz[game.questionIndex].answer;
+        game.currentImage = quiz[game.questionIndex].image;
+        game.questionIndex += 1;
+    }, 4000);
 }
 
 // Start & update timer -->
-function startAndUpdateTimer(game) {
+function startAndUpdateTimer() {
+    game.questionTime = 30;
+    timer.text(game.questionTime);
     game.intervalId = setInterval(function() {
         if (game.questionTime > 0) {
             game.questionTime -= 1;
             timer.text(game.questionTime);
+        } else {
+            clearInterval(game.intervalId);
+            game.unansweredQuestions += 1;
+            gameDisplay.empty();
+            gameDisplay.html(`
+                <h3 class="mb-3">Times Up!</h3>
+                <img 
+                    class="img-fluid rounded answer-image" 
+                    src="${game.currentImage}" 
+                    alt="${game.currentAnswer}" />
+            `);
+            checkGameEnd();
+            if (!game.over) {
+                displayQuestionInFive();                
+            } else {
+                setTimeout(showGameEndResults, 4000);
+            }
         }
     }, 1000);
 }
 
+// Evaluate answer -->
+function evaluateAnswer(answer) {
+    let message;
+    clearInterval(game.intervalId);
+    if (answer === game.currentAnswer) {
+        game.correctAnswers += 1;
+        message = "Correct";
+    } else {
+        game.incorrectAnswers += 1;
+        message = "Incorrect";
+    }
+    gameDisplay.empty();
+    gameDisplay.html(`
+        <h3 class="mb-3">${message}</h3>
+        <img
+            class="img-fluid rounded answer-image"
+            src="${game.currentImage}" 
+            alt="${game.currentAnswer}" />
+    `);
+    checkGameEnd();
+    if (!game.over) {        
+        displayQuestionInFive();
+    } else {
+        setTimeout(showGameEndResults, 4000);
+    }
+}
+
 // PROCESS
 // -------------------------------------------------->
-$(".start").on("click", function(e) {
+// Start trivia game event -->
+startButton.on("click", function(e) {
     $(this).hide();
     timer.show();
-    createAndDisplay(quiz[1]);
+    createAndDisplay(quiz[game.questionIndex]);
     startAndUpdateTimer(game);
+    game.currentAnswer = quiz[game.questionIndex].answer;
+    game.currentImage = quiz[game.questionIndex].image;
+    game.questionIndex += 1;
+});
+
+// Click answer event -->
+gameDisplay.on("click", ".answer", function(e) {
+    if (!game.over) {
+        evaluateAnswer($(this).attr("data-answer"));
+    }
+});
+
+// Click play again event -->
+gameDisplay.on("click", ".play-again", function(e) {
+    gameReset();
+    createAndDisplay(quiz[game.questionIndex]);
+    startAndUpdateTimer(game);
+    game.currentAnswer = quiz[game.questionIndex].answer;
+    game.currentImage = quiz[game.questionIndex].image;
+    game.questionIndex += 1;
 });
